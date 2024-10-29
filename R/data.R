981af9290fc98909d90b7043310be93e1269a813
Idalmk <s215029@dtu.dk>
2024-10-29 09:58:08 +0100

    Changes made by Ida

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' The central dogma of molecular biology
+#'
+#' This package will replicate the central dogma of molecular biology
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
+
+
